110cb2c6f529b2e0bba401150ee77067191d0db7
Ilya <ilya90146@mail.ru>
2019-04-06 14:44:18 +0300

    update view

--- ConsumerBehavior/ConsumerBehavior/ViewModels/Function.cs
+++ ConsumerBehavior/ConsumerBehavior/ViewModels/Function.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace ConsumerBehavior.ViewModels
 {
     class Function
     {
         public string Name { get; set; }
+        public BitmapSource Source { get; set; }
+        //public string Source { get; set; }
     }
 }
--- ConsumerBehavior/ConsumerBehavior/ViewModels/MainWindowViewModel.cs
+++ ConsumerBehavior/ConsumerBehavior/ViewModels/MainWindowViewModel.cs
@@ -6,25 +6,67 @@ using System.Windows.Input;
 //using ConsumerBehavior.Models;
 using ConsumerBehavior.Command;
 using System.Collections.Generic;
+using WpfMath;
+using System.Text;
+using WpfMath.Converters;
 
 namespace ConsumerBehavior.ViewModels
 {
     class MainWindowViewModel : BasicViewModel
     {
-        private string _caption = "Решение задачи оптимального поведения потребителя";
+       
 
-        public ObservableCollection<Function> Functions = new ObservableCollection<Function>();
+        private ObservableCollection<Function> _functions;
 
-        private Function _selectedFunction;
+        public ObservableCollection<Function> Functions
+        {
+            get { return _functions; }
+            set
+            {
+                _functions = value;
+                OnPropertyChanged(nameof(Functions));
+
+            }
+        }
+
 
         public MainWindowViewModel()
         {
-            var names = new string[3] { "Выберите функцию", "Аддитивная", "Логарифмическая" };
+            Functions = new ObservableCollection<Function>();
+            var names = new string[4] { "Аддитивная", "Логарифмическая", "Квадратичная", "Мультипликативная" };
             foreach (var name in names)
             {
-                Functions.Add(new Function() { Name = name });
+                string latex = @"\text{" + name + " — }" + @"U(x)=\sum_{j=1}^{n}\alpha_{j}ln(x_{j})";
+
+                var parser = new TexFormulaParser();
+                var formula = parser.Parse(latex);
+                //var renderer = formula.GetRenderer(TexStyle.Display, 15.0, "Arial");
+                //var geometry = renderer.RenderToGeometry(0, 0);
+                //var converter = new SVGConverter();
+                //var svgPathText = converter.ConvertGeometry(geometry);
+                //var svgText = AddSVGHeader(svgPathText);
+
+                var renderer = formula.GetRenderer(TexStyle.Display, 15.0, "Arial");
+                var bitmapSource = renderer.RenderToBitmap(0.0, 0.0);
+
+
+                Functions.Add(new Function() { Name = name, Source = bitmapSource });
             }
+            
         }
+
+        private string AddSVGHeader(string svgText)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
+                .AppendLine("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" >")
+                .AppendLine(svgText)
+                .AppendLine("</svg>");
+
+            return builder.ToString();
+        }
+
+        private Function _selectedFunction;
 
         public Function SelectedFunction
         {
@@ -36,18 +78,7 @@ namespace ConsumerBehavior.ViewModels
             }
         }
 
-        private RelayCommand _onLoad;
-
-        public RelayCommand OnLoad
-        {
-            get
-            {
-                return _onLoad ?? (_onLoad = new RelayCommand(param => 
-                {
-                   
-                }));
-            }
-        }
+        private string _caption = "Решение задачи оптимального поведения потребителя";
 
         public string Caption
         {
@@ -58,6 +89,24 @@ namespace ConsumerBehavior.ViewModels
                 OnPropertyChanged(nameof(Caption));
             }
         }
+
+
+        //private ICommand _onLoad;
+
+        //public ICommand OnLoad
+        //{
+        //    get
+        //    {
+        //        if (_onLoad == null)
+        //        {
+        //            _onLoad =  new RelayCommand(param =>
+        //            {
+
+        //            });
+        //        }
+        //        return _onLoad;
+        //    }
+        //}
     }
 
 
